74de9fbf31f6039a292a0424b34293265fb2f320
RunningCoderLee <sprint_l@aliyun.com>
2019-08-20 16:31:16 +0800

    feat: support px to viewport

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   plugins: [
-    require('autoprefixer')
+    require('autoprefixer'),
+    require('postcss-px-to-viewport')({
+      unitToConvert: 'px',
+      viewportWidth: 750,
+      unitPrecision: 5,
+      propList: ['*'],
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      selectorBlackList: [],
+      minPixelValue: 1,
+      mediaQuery: true,
+      replace: true,
+      exclude: [/node_modules/],
+      landscape: false,
+      landscapeUnit: 'vw',
+      landscapeWidth: 568,
+    })
   ]
 }
